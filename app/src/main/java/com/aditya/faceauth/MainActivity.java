6afffebdab4dcb6b8b03fa320e50ea475de9ac53
adityabhamare10 <adityabhamare71@gmail.com>
2024-06-25 16:08:30 +0530

    CameraX update

--- app/src/main/java/com/aditya/faceauth/MainActivity.java
+++ app/src/main/java/com/aditya/faceauth/MainActivity.java
@@ -1,8 +1,9 @@
 package com.aditya.faceauth;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
-
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,13 +17,16 @@ import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+import androidx.lifecycle.LifecycleOwner;
 import com.google.common.util.concurrent.ListenableFuture;
-
 import java.io.File;
+import java.util.Date;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
 
     PreviewView previewView;
     ImageCapture imageCapture;
@@ -38,69 +42,87 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-
         previewView = findViewById(R.id.previewView);
 
-        startCamera();
 
-        findViewById(R.id.captureBtn).setOnClickListener(v -> takePhoto());
-
-    }
-
-    private void startCamera() {
-        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
-
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
         cameraProviderFuture.addListener(() -> {
             try {
                 ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-
-                // Preview
-                Preview preview = new Preview.Builder().build();
-                preview.setSurfaceProvider(previewView.getSurfaceProvider());
-
-                // Image Capture
-                imageCapture = new ImageCapture.Builder().build();
-
-                // Select back camera
-                CameraSelector cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA;
-
-                cameraProvider.unbindAll();
-                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture);
-            } catch (ExecutionException | InterruptedException e) {
-                // Handle any errors (including cancellation) here.
+                startCameraX(cameraProvider);
+            }
+            catch (ExecutionException e){
+                Log.d("Error", "Errrrrrrrrorrrr");
                 e.printStackTrace();
             }
-        }, ContextCompat.getMainExecutor(this));
-    }
-
-    private void takePhoto() {
-        if (imageCapture == null) {
-            return;
-        }
-
-        File photoFile = new File(getExternalFilesDir(null), "face.jpg");
-
-        ImageCapture.OutputFileOptions outputOptions = new ImageCapture.OutputFileOptions.Builder(photoFile).build();
-
-        imageCapture.takePicture(outputOptions, ContextCompat.getMainExecutor(this), new ImageCapture.OnImageSavedCallback() {
-            @Override
-            public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
-                // Photo capture succeeded
-                Toast.makeText(MainActivity.this, "Photo capture succeeded: " + photoFile.getAbsolutePath(), Toast.LENGTH_SHORT).show();
-
-//                uploadImageToServer(photoFile);
+            catch (InterruptedException e){
+                Log.d("Error2", "Errorrr2222");
+                e.printStackTrace();
             }
 
+        }, getExecutor());
+
+        findViewById(R.id.captureBtn).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onError(@NonNull ImageCaptureException exception) {
-                // Photo capture failed
-                Toast.makeText(MainActivity.this, "Photo capture failed: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                capturePhoto();
             }
         });
+
     }
 
-//    private void uploadImageToServer(File photoFile) {
+    private Executor getExecutor(){
+        return ContextCompat.getMainExecutor(this);
+    }
 
-//    }
+    public void startCameraX(ProcessCameraProvider cameraProvider){
+        cameraProvider.unbindAll();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        Preview preview = new Preview.Builder().build();
+
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        imageCapture = new ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                .build();
+
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview, imageCapture);
+
+    }
+    private void capturePhoto() {
+
+        File photoDir = new File("C:/Users/hp/Downloads/StoryVerse/CameraX");
+
+        if(!photoDir.exists()){
+            photoDir.mkdir();
+        }
+
+        Date date = new Date();
+        String timeStamp = String.valueOf(date.getTime());
+        String photoFilePath = photoDir.getAbsolutePath() + "/" + timeStamp + ".jpg";
+
+        File photoFile = new File(photoFilePath);
+
+        imageCapture.takePicture(
+                new ImageCapture.OutputFileOptions.Builder(photoFile).build(),
+                getExecutor(),
+                new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                        Toast.makeText(MainActivity.this, "Photo has been Saved Successfully", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exception) {
+                        Toast.makeText(MainActivity.this, "Error: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
+    }
 
 }
